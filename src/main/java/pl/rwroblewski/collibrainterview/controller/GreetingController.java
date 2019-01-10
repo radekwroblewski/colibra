61eaaa4f366917b81e17dbda839a19d95056e92c
Radek Wróblewski <wroblewski.radek@gmail.com>
2019-01-10 20:09:47 +0100

    baeldung approach

--- src/main/java/pl/rwroblewski/collibrainterview/controller/GreetingController.java
+++ src/main/java/pl/rwroblewski/collibrainterview/controller/GreetingController.java
@@ -1,4 +1,6 @@
 package pl.rwroblewski.collibrainterview.controller;
+
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -7,12 +9,17 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 public class GreetingController {
 
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public String greeting(String message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return "Hello, " + HtmlUtils.htmlEscape(message) + "!";
+	}
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public String greeting(String message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return "Hello, " + HtmlUtils.htmlEscape(message) + "!";
-    }
+	@MessageMapping("/chat")
+	@SendTo("/topic/messages")
+	public String send(Message<String> message) throws Exception {
+		return "GO AWAY";
+	}
 
 }
